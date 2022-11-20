1fe15f41d0146d67fb45298329df084355796467
Bùi Văn Tiến <tienvanbui1982001@gmail.com>
2022-11-20 10:54:59 +0700

    task2011-fix-bug-user-feature

---
+++ app/config/defineUrl.php
@@ -0,0 +1,7 @@
+<?php 
+
+define('BASE_URL','http://localhost/PHP2020/mvc196/');
+define('LOGIN_URL_DEFAULT','http://localhost/PHP2020/mvc196/login/index');
+define('IMGS_URL','../public/imgs/');
+define('DASHBOARD_URL_DEFAULT','http://localhost/PHP2020/mvc196/login/dashboard');
+?>
---
+++ app/models/orderModel.php
@@ -0,0 +1,68 @@
+<?php
+
+class orderModel extends Connection
+{
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+  public function insertToOrderTable($table, $data)
+  {
+    return $this->db->insert($table, $data);
+  }
+  public function CountRowToCheckIsset($table, $id)
+  {
+    $sql = "SELECT * FROM $table WHERE `$table`.`cart_id` = $id";
+    return $this->db->getNumbersRow($sql);
+  }
+  public function selectproductListInOrder($table, $tableProduct, $tableOrder, $collum, $id)
+  {
+    $sql = "SELECT * FROM $table INNER JOIN $tableProduct ON `$table`.`product_id` = `$tableProduct`.`id`  INNER JOIN $tableOrder ON `$table`.`order_id` = `$tableOrder`.`id` WHERE `$table`.`$collum` = $id ";
+    return $this->db->select($sql);
+  }
+  public function countProductNumberInOrder($tableOrder, $tableOrderDetail, $collum, $id)
+  {
+    $sql = "SELECT * FROM $tableOrder 
+    INNER JOIN $tableOrderDetail ON `$tableOrder`.`id` = `$tableOrderDetail`.`order_id`  
+    WHERE `$tableOrderDetail`.`$collum` = $id AND `$tableOrder`.`status` = 1";
+    return $this->db->getNumbersRow($sql);
+  }
+  public function selectOrderItem($table, $collum, $id)
+  {
+    $sql = "SELECT * FROM $table WHERE `$table`.`$collum` = $id";
+    return $this->db->select($sql);
+  }
+  public function deleteOrder($table, $conditon)
+  {
+    return $this->db->delete($table, $conditon);
+  }
+  public function updateStatusOfOrder($table, $data, $conditon)
+  {
+    return $this->db->update($table, $data, $conditon);
+  }
+  public function selectOrderTracking( $tableOrder,$tablePayment)
+  {
+    $sql = "SELECT * FROM `$tableOrder` 
+    INNER JOIN $tablePayment ON `$tableOrder`.`payment_id` = `$tablePayment`.`id`
+    ";
+    return $this->db->select($sql);
+  }
+  public function sumOrderTotalPrice($table, $tableOrderDetail, $tableCart, $user_id, $tableProduct)
+  {
+    $sql = "SELECT SUM(`$tableOrderDetail`.`total_price`) as `subTotal` FROM `$table` INNER JOIN $tableOrderDetail ON `$table`.`id` = `$tableOrderDetail`.`order_id` INNER JOIN $tableCart ON `$tableCart`.`id` = `$table`.`cart_id` INNER JOIN $tableProduct ON `$tableOrderDetail`.`product_id` = `$tableProduct`.`id` WHERE `$tableCart`.`user_id` = $user_id  ";
+    return $this->db->select($sql);
+  }
+  public function selectAllOrder($tableOrder, $tableOrderDetail, $tableProduct, $tablePayment, $tableUser, $tableCart)
+  {
+    $sql = "SELECT `$tableOrder`.`id` as order_id, `$tableOrder`.`status` as status, `$tableUser`.`name` as order_user, total_price, `$tablePayment`.`name` as payment_name, `$tableOrder`.`created_date` as created_date FROM $tableOrder 
+    INNER JOIN $tableCart ON `$tableCart`.`id` = `$tableOrder`.`cart_id`
+    INNER JOIN $tableOrderDetail ON `$tableOrderDetail`.`order_id` = `$tableOrder`.`id`
+    INNER JOIN $tableProduct ON `$tableOrderDetail`.`product_id` = `$tableProduct`.`id`
+    INNER JOIN $tablePayment ON `$tablePayment`.`id` = `$tableOrder`.`payment_id`
+    INNER JOIN $tableUser ON `$tableUser`.`id` = `$tableCart`.`user_id`
+    GROUP BY `$tableOrderDetail`.`order_id`;
+    ";
+    return $this->db->select($sql);
+  }
+}
